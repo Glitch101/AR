fb4476eda4ec135d37df96c541ed9cedb0d7e5ca
Glitch101 <no1knowsmypwd>
2019-12-30 23:10:42 +0530

    Files updated

---
+++ app/src/main/java/com/example/shoparx/MainActivity.java
@@ -0,0 +1,339 @@
+package com.example.shoparx;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Camera;
+import android.graphics.ImageFormat;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.SystemClock;
+import android.util.Log;
+import android.util.Rational;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageAnalysisConfig;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.time.Clock;
+
+import id.zelory.compressor.Compressor;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    TextureView textureView;
+    private File mFile;
+    public static final MediaType MEDIA_TYPE_IMAGE
+            = MediaType.parse("image/jpg");
+    private final OkHttpClient client = new OkHttpClient();
+    /**
+     * An additional thread for running tasks that shouldn't block the UI.
+     */
+    private HandlerThread mBackgroundThread;
+
+    /**
+     * A {@link Handler} for running tasks in the background.
+     */
+    private Handler mBackgroundHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textureView = findViewById(R.id.view_finder);
+        mFile = new File(this.getExternalFilesDir(null), "pic.jpg");
+        startBackgroundThread();
+        try {
+            startCamera();
+        } catch(Exception exc) {
+            Log.i("TAG", exc.getMessage());
+        }
+    }
+
+    private void startCamera() {
+        CameraX.unbindAll();
+        mFile = new File(this.getExternalFilesDir(null), "pic.jpg");
+        Rational aspectRatio = new Rational(textureView.getWidth(), textureView.getHeight());
+        Size screen = new Size(textureView.getWidth(), textureView.getHeight()); //size of the screen
+
+
+        PreviewConfig pConfig = new PreviewConfig.Builder().setTargetAspectRatio(aspectRatio).setTargetResolution(screen).build();
+        Preview preview = new Preview(pConfig);
+
+        ImageAnalysisConfig config =
+                new ImageAnalysisConfig.Builder()
+                        .setTargetResolution(new Size(1280, 720))
+                        .setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+                        .setCallbackHandler(mBackgroundHandler)
+                        .build();
+        ImageAnalysis imageAnalysis = new ImageAnalysis(config);
+
+        try {
+            imageAnalysis.setAnalyzer(
+                    new ImageAnalysis.Analyzer() {
+                        @Override
+                        public void analyze(ImageProxy imageProxy, int rotationDegrees) {
+                            sendFrame(imageProxy.getImage());
+                            SystemClock.sleep(200);
+                        }
+                    }
+            );
+        } catch(Exception exc) {
+            Log.i("TAG", exc.getMessage());
+        }
+
+        preview.setOnPreviewOutputUpdateListener(
+                new Preview.OnPreviewOutputUpdateListener() {
+                    //to update the surface texture we  have to destroy it first then re-add it
+                    @Override
+                    public void onUpdated(Preview.PreviewOutput output){
+                        ViewGroup parent = (ViewGroup) textureView.getParent();
+                        parent.removeView(textureView);
+                        parent.addView(textureView, 0);
+
+                        textureView.setSurfaceTexture(output.getSurfaceTexture());
+                        updateTransform();
+                    }
+                });
+
+//        ImageCaptureConfig imageCaptureConfig = new ImageCaptureConfig.Builder().setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+//                .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
+//        final ImageCapture imgCap = new ImageCapture(imageCaptureConfig);
+
+//        findViewById(R.id.imgCapture).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                File file = new File(Environment.getExternalStorageDirectory() + "/" + System.currentTimeMillis() + ".png");
+//                imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
+//                    @Override
+//                    public void onImageSaved(@NonNull File file) {
+//                        String msg = "Pic captured at " + file.getAbsolutePath();
+//                        Toast.makeText(getBaseContext(), msg,Toast.LENGTH_LONG).show();
+//                    }
+//
+//                    @Override
+//                    public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
+//                        String msg = "Pic capture failed : " + message;
+//                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+//                        if(cause != null){
+//                            cause.printStackTrace();
+//                        }
+//                    }
+//                });
+//            }
+//        });
+
+        //bind to lifecycle:
+        CameraX.bindToLifecycle((LifecycleOwner)this, imageAnalysis, preview);
+    }
+
+    private void startBackgroundThread() {
+        mBackgroundThread = new HandlerThread("CameraBackground");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    /**
+     * Stops the background thread and its {@link Handler}.
+     */
+    private void stopBackgroundThread() {
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void sendFrame(Image mImage) {
+
+        try {
+            byte[] bytes = NV21toJPEG(YUV420toNV21(mImage), mImage.getWidth(), mImage.getHeight(), 100);
+            FileOutputStream output = null;
+            output = new FileOutputStream(mFile);
+            output.write(bytes);
+            //compressedFile = new Compressor(mContext).compressToFile(mFile);
+            // TODO: Send captured image to server
+            RequestBody postBodyImage = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("image", "imageFromAndroid.jpg", RequestBody.create(mFile, MediaType.parse("image/*jpg")))
+                    .build();
+//                RequestBody body = RequestBody.create(mFile, MEDIA_TYPE_IMAGE);
+            Request request = new Request.Builder()
+                    .url("http://192.168.1.2:5000/upload")
+                    .post(postBodyImage)
+                    .build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    String mMessage = e.getMessage().toString();
+                    Log.i("TAG", mMessage + " is the error");
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    String mMessage = response.body().string();
+                    Log.i("TAG", mMessage);
+                }
+            });
+//
+        } catch(Exception exc) {
+            Log.i("TAG", "EXCEPTION CAUGHT" + exc.getMessage());
+        }
+    }
+
+
+    private static byte[] NV21toJPEG(byte[] nv21, int width, int height, int quality) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        YuvImage yuv = new YuvImage(nv21, ImageFormat.NV21, width, height, null);
+        yuv.compressToJpeg(new Rect(0, 0, width, height), quality, out);
+        return out.toByteArray();
+    }
+
+    private static byte[] YUV420toNV21(Image image) {
+        Rect crop = image.getCropRect();
+        int format = image.getFormat();
+        int width = crop.width();
+        int height = crop.height();
+        Image.Plane[] planes = image.getPlanes();
+        byte[] data = new byte[width * height * ImageFormat.getBitsPerPixel(format) / 8];
+        byte[] rowData = new byte[planes[0].getRowStride()];
+
+        int channelOffset = 0;
+        int outputStride = 1;
+        for (int i = 0; i < planes.length; i++) {
+            switch (i) {
+                case 0:
+                    channelOffset = 0;
+                    outputStride = 1;
+                    break;
+                case 1:
+                    channelOffset = width * height + 1;
+                    outputStride = 2;
+                    break;
+                case 2:
+                    channelOffset = width * height;
+                    outputStride = 2;
+                    break;
+            }
+
+            ByteBuffer buffer = planes[i].getBuffer();
+            int rowStride = planes[i].getRowStride();
+            int pixelStride = planes[i].getPixelStride();
+
+            int shift = (i == 0) ? 0 : 1;
+            int w = width >> shift;
+            int h = height >> shift;
+            buffer.position(rowStride * (crop.top >> shift) + pixelStride * (crop.left >> shift));
+            for (int row = 0; row < h; row++) {
+                int length;
+                if (pixelStride == 1 && outputStride == 1) {
+                    length = w;
+                    buffer.get(data, channelOffset, length);
+                    channelOffset += length;
+                } else {
+                    length = (w - 1) * pixelStride + 1;
+                    buffer.get(rowData, 0, length);
+                    for (int col = 0; col < w; col++) {
+                        data[channelOffset] = rowData[col * pixelStride];
+                        channelOffset += outputStride;
+                    }
+                }
+                if (row < h - 1) {
+                    buffer.position(buffer.position() + rowStride - length);
+                }
+            }
+        }
+        return data;
+    }
+
+    private void updateTransform(){
+        Matrix mx = new Matrix();
+        float w = textureView.getMeasuredWidth();
+        float h = textureView.getMeasuredHeight();
+
+        float cX = w / 2f;
+        float cY = h / 2f;
+
+        int rotationDgr;
+        int rotation = (int)textureView.getRotation();
+
+        switch(rotation){
+            case Surface.ROTATION_0:
+                rotationDgr = 0;
+                break;
+            case Surface.ROTATION_90:
+                rotationDgr = 90;
+                break;
+            case Surface.ROTATION_180:
+                rotationDgr = 180;
+                break;
+            case Surface.ROTATION_270:
+                rotationDgr = 270;
+                break;
+            default:
+                return;
+        }
+
+        mx.postRotate((float)rotationDgr, cX, cY);
+        textureView.setTransform(mx);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopBackgroundThread();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+//        startBackgroundThread();
+    }
+
+
+}
+
+
+
